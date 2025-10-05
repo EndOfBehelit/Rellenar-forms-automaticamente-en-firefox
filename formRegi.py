8871cc5673e2ff559fc606a714d9c39a96f4ef00
EndOfBehelit <154753826+EndOfBehelit@users.noreply.github.com>
2025-10-05 15:10:14 +0200

    Update formRegi.py

--- formRegi.py
+++ formRegi.py
@@ -8,14 +8,14 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 # ruta a geckodriver si no está en PATH
-service = Service(r"C:\Users\Carba\Downloads\geckodriver-v0.36.0-win32\geckodriver.exe")
+service = Service(r"C:\Users\USER\Downloads\geckodriver-v0.36.0-win32\geckodriver.exe")
 
 opts = Options()
 opts.binary_location = r"C:\Program Files\Mozilla Firefox\firefox.exe"
 opts.add_argument("--headless") # No abre la ventana de firefox
 
 driver = webdriver.Firefox(service=service, options=opts)
-driver.get("https://docs.google.com/forms/d/1cnzlScPsOeQBWDC65IPc7caZGXQmTP4JzNEk1AAdJ7E/edit")
+driver.get("RUTA FORMS")
 
 wait = WebDriverWait(driver, 10) #Esto sirve a que de tiempo a cargar la pagina, sino puede dar erorr al no encontrar algo que aun no ha cargado
 
@@ -29,34 +29,33 @@ time.sleep(0.5)  # medio segundo
 correo = driver.find_element(By.XPATH, '(//input[@jsname="YPqjbf"])[2]')
 correo.send_keys("Nombre de prueba")
 
-# Cansancio
+# Checkbox1
 opcion = driver.find_element(By.XPATH, '(//div[@role="checkbox" and @aria-label="2"])[1]')
 opcion.click()
 
-# Dormir
+# Checkbox2
 opcion = driver.find_element(By.XPATH, '(//div[@role="checkbox" and @aria-label="10"])[2]')
 opcion.click()
 
-# Comer
+# Checkbox3
 opcion = driver.find_element(By.XPATH, '(//div[@role="checkbox" and @aria-label="10"])[3]')
 opcion.click()
 
-# Lesion hoy
-time.sleep(1)  # medio segundo
+# texto1
+time.sleep(1)  # un segundo
 correo = driver.find_element(By.XPATH, '(//textarea[@jsname="YPqjbf"])[1]')
 correo.send_keys("Texto de prueba")
-#correo.send_keys("Sin lesion")
 
-# Lesion pasada
+# Texto 2
 time.sleep(0.5)  # medio segundo
 correo = driver.find_element(By.XPATH, '(//textarea[@jsname="YPqjbf"])[2]')
 correo.send_keys("Texto de prueba 2")
-#correo.send_keys("Sin lesion pasada")
 
 # Botón enviar
 time.sleep(0.5)  # medio segundo
 submit = driver.find_element(By.XPATH, '//div[@role="button" and @aria-label="Submit"]')
 submit.click()
 
-# después de usar:
+# Cerrar firefox
 driver.quit()
+
