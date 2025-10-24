e315357b11aec98651265c7680ef8d2e09c27171
EndOfBehelit <154753826+EndOfBehelit@users.noreply.github.com>
2025-10-25 01:38:41 +0200

    Add files via upload

--- formRegi.py
+++ formRegi.py
@@ -7,55 +7,62 @@ from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-# ruta a geckodriver si no está en PATH
-service = Service(r"C:\Users\USER\Downloads\geckodriver-v0.36.0-win32\geckodriver.exe")
+try:
+    # ruta a geckodriver si no está en PATH
+    service = Service(r"C:\Users\Carba\OneDrive - Universidad de Oviedo\Escritorio\Test rellenar form\geckodriver-v0.36.0-win32\geckodriver.exe")
 
-opts = Options()
-opts.binary_location = r"C:\Program Files\Mozilla Firefox\firefox.exe"
-opts.add_argument("--headless") # No abre la ventana de firefox
+    opts = Options()
+    opts.binary_location = r"C:\Program Files\Mozilla Firefox\firefox.exe"
+    opts.add_argument("--headless") # No abre la ventana de firefox
 
-driver = webdriver.Firefox(service=service, options=opts)
-driver.get("RUTA FORMS")
+    driver = webdriver.Firefox(service=service, options=opts)
+    driver.get("https://docs.google.com/forms/d/1cnzlScPsOeQBWDC65IPc7caZGXQmTP4JzNEk1AAdJ7E/edit")
 
-wait = WebDriverWait(driver, 10) #Esto sirve a que de tiempo a cargar la pagina, sino puede dar erorr al no encontrar algo que aun no ha cargado
+    wait = WebDriverWait(driver, 10) #Esto sirve a que de tiempo a cargar la pagina, sino puede dar erorr al no encontrar algo que aun no ha cargado
 
-# Correo
-time.sleep(0.5)  # medio segundo
-correo = driver.find_element(By.XPATH, '(//input[@jsname="YPqjbf"])[1]')
-correo.send_keys("mi_correo@test.com")
+    print("Rellenando formulario...")
+    # Correo
+    time.sleep(0.5)  # medio segundo
+    correo = driver.find_element(By.XPATH, '(//input[@jsname="YPqjbf"])[1]')
+    correo.send_keys("jorcarmor@hotmail.com")
 
-# Nombre
-time.sleep(0.5)  # medio segundo
-correo = driver.find_element(By.XPATH, '(//input[@jsname="YPqjbf"])[2]')
-correo.send_keys("Nombre de prueba")
+    # Nombre
+    time.sleep(0.5)  # medio segundo
+    correo = driver.find_element(By.XPATH, '(//input[@jsname="YPqjbf"])[2]')
+    correo.send_keys("Carba")
 
-# Checkbox1
-opcion = driver.find_element(By.XPATH, '(//div[@role="checkbox" and @aria-label="2"])[1]')
-opcion.click()
+    # Cansancio
+    opcion = driver.find_element(By.XPATH, '(//div[@role="checkbox" and @aria-label="2"])[1]')
+    opcion.click()
 
-# Checkbox2
-opcion = driver.find_element(By.XPATH, '(//div[@role="checkbox" and @aria-label="10"])[2]')
-opcion.click()
+    # Dormir
+    opcion = driver.find_element(By.XPATH, '(//div[@role="checkbox" and @aria-label="10"])[2]')
+    opcion.click()
 
-# Checkbox3
-opcion = driver.find_element(By.XPATH, '(//div[@role="checkbox" and @aria-label="10"])[3]')
-opcion.click()
+    # Comer
+    opcion = driver.find_element(By.XPATH, '(//div[@role="checkbox" and @aria-label="10"])[3]')
+    opcion.click()
 
-# texto1
-time.sleep(1)  # un segundo
-correo = driver.find_element(By.XPATH, '(//textarea[@jsname="YPqjbf"])[1]')
-correo.send_keys("Texto de prueba")
+    # Lesion hoy
+    time.sleep(1)  # medio segundo
+    correo = driver.find_element(By.XPATH, '(//textarea[@jsname="YPqjbf"])[1]')
+    correo.send_keys("No")
+    #correo.send_keys("Sin lesion")
 
-# Texto 2
-time.sleep(0.5)  # medio segundo
-correo = driver.find_element(By.XPATH, '(//textarea[@jsname="YPqjbf"])[2]')
-correo.send_keys("Texto de prueba 2")
+    # Lesion pasada
+    time.sleep(0.5)  # medio segundo
+    correo = driver.find_element(By.XPATH, '(//textarea[@jsname="YPqjbf"])[2]')
+    correo.send_keys("No")
+    #correo.send_keys("Sin lesion pasada")
 
-# Botón enviar
-time.sleep(0.5)  # medio segundo
-submit = driver.find_element(By.XPATH, '//div[@role="button" and @aria-label="Submit"]')
-submit.click()
-
-# Cerrar firefox
+    # Botón enviar
+    time.sleep(0.5)  # medio segundo
+    submit = driver.find_element(By.XPATH, '//div[@role="button" and @aria-label="Submit"]')
+    submit.click()
+    
+except:
+    print("Ha ocurrido algún error.")
+else:
+    print("Todo rellenado sin problemas")
+# cerrar explorador
 driver.quit()
-
